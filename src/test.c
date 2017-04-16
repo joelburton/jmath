914d8cd05a0c5a3ca8e0b66f93a7503a6343ebfd
Joel Burton <joel@joelburton.com>
2017-04-16 04:07:13 +0000

    Removed old make file.

--- src/test.c
+++ src/test.c
@@ -1,16 +1,13 @@
 #include <glib.h>
 #include "math.h"
 
-typedef struct {
-	int x;
-	int y;
-} fixture;
+typedef struct {} fixture;
 
-void test_add(fixture *f, gconstpointer ignored) {
+void test_add(fixture *ignored_fixture, gconstpointer ignored) {
 	g_assert(add(10, 20) == 30);
 }
 
-void test_sub(fixture *f, gconstpointer ignored) {
+void test_sub(fixture *ignored_fixture, gconstpointer ignored) {
 	g_assert(sub(20, 10) == 10);
 }
 
